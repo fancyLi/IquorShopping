109885bb66d0985b0b35ef581e606bba05a0f4a7
nanli5 <827752527@qq.com>
2018-05-09 16:24:27 +0800

    Initial Commit

---
+++ IquorShoppingTests/IquorShoppingTests.m
@@ -0,0 +1,39 @@
+//
+//  IquorShoppingTests.m
+//  IquorShoppingTests
+//
+//  Created by nanli5 on 2018/5/9.
+//  Copyright © 2018年 Hefei elevation network technology co. LTD. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface IquorShoppingTests : XCTestCase
+
+@end
+
+@implementation IquorShoppingTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
